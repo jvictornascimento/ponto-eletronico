76e10970a20cf64518fbeec50d9f05f38dcc22c0
jvictornascimento <victojoao789@gmail.com>
2026-06-16 22:47:30 -0300

    feat(settings): add money formatter

---
+++ lib/shared/money/money_formatter.dart
@@ -0,0 +1,19 @@
+class MoneyFormatter {
+  const MoneyFormatter._();
+
+  static String formatCents(int cents) {
+    final reais = cents ~/ 100;
+    final centavos = (cents % 100).toString().padLeft(2, '0');
+
+    return 'R\$ $reais,$centavos';
+  }
+
+  static int parseToCents(String value) {
+    final onlyNumbers = value.replaceAll(RegExp(r'[^0-9]'), '');
+    if (onlyNumbers.isEmpty) {
+      return 0;
+    }
+
+    return int.parse(onlyNumbers);
+  }
+}
